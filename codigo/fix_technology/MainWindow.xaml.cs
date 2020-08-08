bac8406e67cf35a3416469fa89b77443ec9822e9
Angel Sanchez <angelsanchez934@gmail.com>
2020-08-08 16:02:10 -0500

    Se inicio el proyecto en VS
    Adicionalmente se agrego el paquete dao con la clase para la conexión a la base de datos.

---
+++ codigo/fix_technology/MainWindow.xaml.cs
@@ -0,0 +1,55 @@
+﻿using fix_technology.dao;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace fix_technology
+{
+    /// <summary>
+    /// Lógica de interacción para MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        ConexionDb conexion = new ConexionDb();
+        public MainWindow()
+        {
+            InitializeComponent();
+            abrirConexion();
+        }
+
+        private void abrirConexion()
+        {
+            bool dbCconectada = conexion.abrir();
+            if (dbCconectada)
+            {
+                Answer.Content = "Se conecto a la base de datos\n\n";
+            }
+            else
+            {
+                Answer.Content = "No se conecto a la base de datos. Ver resultado en consola.\n\n";
+            }
+
+            bool dbCerrada = conexion.cerrar();
+
+            if (dbCerrada)
+            {
+                Answer2.Content = "Se desconecto la base de datos\n\n";
+            }
+            else
+            {
+                Answer.Content = "No se desconecto la base de datos. Ver resultado en consola.\n\n";
+            }
+        }
+    }
+}
---
+++ codigo/fix_technology/dao/ConexionDb.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Data.SqlClient;
+
+namespace fix_technology.dao
+{
+
+    /// <summary>
+    /// Lógica de interacción para abrir o cerrar la conexion 
+    /// a la base de datos del negocio Fix Technology
+    /// </summary>
+    class ConexionDb
+    {
+        string credenciales = @"Server = localhost; Database = fix_technology; Trusted_Connection = True;";
+        SqlConnection conexion;
+
+        /// <summary>
+        /// Establece una conexión a la base de datos.
+        /// </summary>
+        /// <returns>True: si se esableció la conexión de forma exitosa o False en caso contrario.</returns>
+        /// <exception cref="InvalidOperationException">Lanzada cuando una llamada al método no es válida para el estado actual del objeto.</exception>
+        ///  /// <exception cref="SqlException">Lanzada cuando hay un error en SQL al cerrar la conexión.</exception>
+        /// <exception cref="Exception">Lanzada cuando ocurre un error no reconocido.</exception>
+        public bool abrir()
+        {
+            bool conectada = false;
+
+            try
+            {
+                conexion = new SqlConnection();
+                conexion.ConnectionString = credenciales;
+                conexion.Open();
+                conectada = true;
+            }
+            catch (InvalidOperationException ex)
+            {
+                throw ex;
+            }
+            catch (SqlException ex)
+            {
+                throw ex;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+
+            return conectada;
+        }
+
+        /// <summary>
+        /// Comprueba si la conexión a base de datos ha sido establecida previamente y
+        /// en caso afirmativo, la finaliza.
+        /// </summary>
+        /// <returns>True: si se cerró la conexión de forma exitosa o False en caso contrario.</returns>
+        /// <exception cref="SqlException">Lanzada cuando hay un error en SQL al cerrar la conexión.</exception>
+        /// <exception cref="Exception">Lanzada cuando ocurre un error no reconocido.</exception>
+        public bool cerrar()
+        {
+            bool cerrada = false;
+
+            if (conexion != null)
+            {
+                try
+                {
+                    conexion.Close();
+                    cerrada = true;
+                }
+                catch (SqlException ex)
+                {
+                    throw ex;
+                }
+                catch (Exception ex)
+                {
+                    throw ex;
+                }
+            }
+
+            return cerrada;
+        }
+    }
+}
