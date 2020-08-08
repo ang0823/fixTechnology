de8c545539bc26432057464bc32f46bfc4f5a5c1
Angel Sanchez <angelsanchez934@gmail.com>
2020-08-08 16:26:21 -0500

    Se agrego la interfaz para el contrato

---
+++ codigo/fix_technology/dao/interfaces/ContratoDao.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace fix_technology.dao.interfaces
+{
+    interface ContratoDao
+    {
+        void actualizarContrato(Contrato contrato);
+        List<Contrato> buscarPorNombreCliente(string nombreCliente);
+        List<Contrato> buscarPorFechaDeRecepcion(DateTime fecha);
+        List<Contrato> buscarPorModeloEquipo(string nombreModelo);
+        List<Contrato> buscarPorNumeroDeSerie(string numeroSerie);
+        List<Contrato> buscarPorNumeroDeServicio(string numeroServicio);
+        int obtenerCantidadActualDeContratos();
+        void registrar();
+    }
+}
